065fdde19cf3c590b6458205a4bef6f3b0734151
竹原伝 <tsuto.takehara@nijibox.co.jp>
2019-02-25 01:39:57 +0900

    [Add] Move Repository method

--- app/src/main/java/jp/example/paginglivedata/data/paging/ItemDataSource.kt
+++ app/src/main/java/jp/example/paginglivedata/data/paging/ItemDataSource.kt
@@ -2,22 +2,21 @@ package jp.example.paginglivedata.data.paging
 
 import android.arch.paging.PageKeyedDataSource
 import jp.example.paginglivedata.FIRST_PAGE
+import jp.example.paginglivedata.PAGE_SIZE
+import jp.example.paginglivedata.SITE_NAME
 import jp.example.paginglivedata.data.entity.Item
-import jp.example.paginglivedata.data.repostitory.ItemRepository
+import jp.example.paginglivedata.data.entity.StackApiResponse
+import jp.example.paginglivedata.data.service.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import timber.log.Timber
-import java.util.*
 
 /**
  * 要求が次のページまたは前のページのキーを返す、ページキー付きコンテンツ用のデータローダー。
- * 高階関数で、Repositoryの処理にコールバックを設定する
+ * 高階関数で、処理にコールバックを設定する
  */
 class ItemDataSource : PageKeyedDataSource<Int, Item>() {
-
-    /**
-     * リポジトリークラスのインスタンス
-     */
-    private val itemRepository: ItemRepository = ItemRepository()
-
 
     /**
      * PagedListをデータで初期化するために最初に呼び出される
@@ -27,7 +26,7 @@ class ItemDataSource : PageKeyedDataSource<Int, Item>() {
         /**
          * 最初にロードする処理(First Pageを取得)
          */
-        itemRepository.getItems(FIRST_PAGE) { data ->
+        callApi(FIRST_PAGE) { data ->
             callback.onResult(data.items, null, FIRST_PAGE + 1)
             Timber.d("loadInitial:itemSize=${data.items.size}, currentPageKey=${FIRST_PAGE}, nextPageKey=${FIRST_PAGE + 1}")
         }
@@ -41,7 +40,7 @@ class ItemDataSource : PageKeyedDataSource<Int, Item>() {
         /**
          * 前のページをロードする処理 今回は使っていない
          */
-        itemRepository.getItems(params.key) { data ->
+        callApi(params.key) { data ->
             val adjacentKey = if (params.key > 1) params.key - 1 else null
             callback.onResult(data.items, adjacentKey)
             Timber.d("loadBefore:itemSize=${data.items.size}, currentPageKey=${params.key}, nextPageKey=$adjacentKey")
@@ -57,11 +56,37 @@ class ItemDataSource : PageKeyedDataSource<Int, Item>() {
          * 次のページをロードする処理
          * 初期化時、loadInitial後に前もって1度行われ、ページを最後までスクロールするたびに呼ばれる
          */
-        itemRepository.getItems(params.key) { data ->
+        callApi(params.key) { data ->
             val key = if (data.has_more) params.key + 1 else null
             callback.onResult(data.items, key)
             Timber.d("loadAfter:itemSize=${data.items.size}, currentPageKey=${params.key}, nextPageKey=$key,hasMoreData=${data.has_more}")
 
         }
+    }
+
+    private fun callApi(firstPageIndex: Int, pagingCallback: (StackApiResponse) -> Unit) {
+
+        /**
+         * Retrofit Factoryからシングルトンでインスタンスを取得
+         */
+        RetrofitClient.instance
+            .api.getAnswers(firstPageIndex, PAGE_SIZE, SITE_NAME)
+            .enqueue(object : Callback<StackApiResponse> {
+                /**
+                 * 成功時、各Pagingフェーズのコールバックを受け取る
+                 */
+                override fun onResponse(call: Call<StackApiResponse>, response: Response<StackApiResponse>) {
+
+                    val data = response.body()
+
+                    if (data != null) {
+                        pagingCallback(data)
+                    }
+                }
+
+                override fun onFailure(call: Call<StackApiResponse>, t: Throwable) {
+                    t.printStackTrace()
+                }
+            })
     }
 }
